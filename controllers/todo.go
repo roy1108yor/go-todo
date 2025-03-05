c3a5f3e9152aebd833ffca541d991f656c6b732b
Clacky <develop@clacky.ai>
2025-03-05 07:35:21 -0500

    Add edit functionality to todo tasks with inline editing

--- controllers/todo.go
+++ controllers/todo.go
@@ -1,13 +1,15 @@
 package controllers
 
 import (
- "fmt"
- "github.com/gorilla/mux"
- "github.com/ichtrojan/go-todo/config"
- "github.com/ichtrojan/go-todo/models"
- "html/template"
- "net/http"
- "strings"
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/ichtrojan/go-todo/config"
+	"github.com/ichtrojan/go-todo/models"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"strings"
 )
 
 var (
@@ -59,21 +61,21 @@ func Show(w http.ResponseWriter, r *http.Request) {
 }
 
 func Add(w http.ResponseWriter, r *http.Request) {
- item := r.FormValue("item")
+	item := r.FormValue("item")
 
- // Validate empty input
- if item == "" || len(strings.TrimSpace(item)) == 0 {
-  http.Redirect(w, r, "/", 301)
-  return
- }
+	// Validate empty input
+	if item == "" || len(strings.TrimSpace(item)) == 0 {
+		http.Redirect(w, r, "/", 301)
+		return
+	}
 
- _, err := database.Exec(`INSERT INTO todos (item) VALUE (?)`, item)
+	_, err := database.Exec(`INSERT INTO todos (item) VALUE (?)`, item)
 
- if err != nil {
-  fmt.Println(err)
- }
+	if err != nil {
+		fmt.Println(err)
+	}
 
- http.Redirect(w, r, "/", 301)
+	http.Redirect(w, r, "/", 301)
 }
 
 func Delete(w http.ResponseWriter, r *http.Request) {
@@ -100,4 +102,48 @@ func Complete(w http.ResponseWriter, r *http.Request) {
 	}
 
 	http.Redirect(w, r, "/", 301)
+}
+
+// Update handles updating an existing todo task
+func Update(w http.ResponseWriter, r *http.Request) {
+	// Parse the ID from URL parameters
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	// Read request body for updated task content
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error reading request body", http.StatusBadRequest)
+		return
+	}
+
+	// Parse body as JSON
+	var requestData struct {
+		Item string `json:"item"`
+	}
+	
+	if err := json.Unmarshal(body, &requestData); err != nil {
+		http.Error(w, "Invalid request format", http.StatusBadRequest)
+		return
+	}
+
+	// Validate that the updated task is not empty
+	updatedItem := requestData.Item
+	if updatedItem == "" || len(strings.TrimSpace(updatedItem)) == 0 {
+		http.Error(w, "Task content cannot be empty", http.StatusBadRequest)
+		return
+	}
+
+	// Update the task in the database
+	_, err = database.Exec(`UPDATE todos SET item = ? WHERE id = ?`, updatedItem, id)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Failed to update task", http.StatusInternalServerError)
+		return
+	}
+
+	// Return success response
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{"status": "success", "message": "Task updated successfully"})
 }
--- routes/web.go
+++ routes/web.go
@@ -12,6 +12,7 @@ func Init() *mux.Router {
 	route.HandleFunc("/add", controllers.Add).Methods("POST")
 	route.HandleFunc("/delete/{id}", controllers.Delete)
 	route.HandleFunc("/complete/{id}", controllers.Complete)
+	route.HandleFunc("/update/{id}", controllers.Update).Methods("POST")
 
 	return route
 }
